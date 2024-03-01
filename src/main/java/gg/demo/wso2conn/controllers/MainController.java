e3809a9b266c40ed4258b80288d941ff2f6092b1
Guilherme R Rosa <ggcodigos@gmail.com>
2024-02-29 23:47:15 -0300

    Added OAUTH2 authentication with asgardeo

--- src/main/java/gg/demo/wso2conn/controllers/MainController.java
+++ src/main/java/gg/demo/wso2conn/controllers/MainController.java
@@ -1,15 +1,22 @@
 package gg.demo.wso2conn.controllers;
 
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/dev")
 public class MainController {
 
     @GetMapping
-    public String getIndex(){
+    public String getIndex(Principal principal, Model model){
+        String username = principal.getName();
+
+        model.addAttribute("username", username);
         return "index";
     }
 }
